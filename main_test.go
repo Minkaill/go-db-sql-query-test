a59291d28e2337b7537a066107f7fefc39f8a6e6
Minkaill <mkx.dake@gmail.com>
2025-09-26 14:43:30 +0300

    finish

--- main_test.go
+++ main_test.go
@@ -1,29 +1,59 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	_ "modernc.org/sqlite"
 )
 
 func Test_SelectClient_WhenOk(t *testing.T) {
-	// настройте подключение к БД
+	db, err := sql.Open("sqlite", "demo.db")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	clientID := 1
 
-	// напиши тест здесь
+	cl, err := selectClient(db, clientID)
+
+	require.NoError(t, err)
+	require.Equal(t, clientID, cl.ID)
+	assert.NotEmpty(t, cl.FIO)
+	assert.NotEmpty(t, cl.Login)
+	assert.NotEmpty(t, cl.Birthday)
+	assert.NotEmpty(t, cl.Email)
 }
 
 func Test_SelectClient_WhenNoClient(t *testing.T) {
-	// настройте подключение к БД
+	db, err := sql.Open("sqlite", "demo.db")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	clientID := -1
 
-	// напиши тест здесь
+	cl, err := selectClient(db, clientID)
+
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	assert.Empty(t, cl.FIO)
+	assert.Empty(t, cl.Login)
+	assert.Empty(t, cl.Birthday)
+	assert.Empty(t, cl.Email)
 }
 
 func Test_InsertClient_ThenSelectAndCheck(t *testing.T) {
-	// настройте подключение к БД
+	db, err := sql.Open("sqlite", "demo.db")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	cl := Client{
 		FIO:      "Test",
@@ -32,11 +62,28 @@ func Test_InsertClient_ThenSelectAndCheck(t *testing.T) {
 		Email:    "mail@mail.com",
 	}
 
-	// напиши тест здесь
+	rowID, err := insertClient(db, cl)
+	require.NoError(t, err)
+	require.NotEmpty(t, rowID)
+
+	cl.ID = rowID
+
+	client, err := selectClient(db, rowID)
+	require.NoError(t, err)
+
+	assert.Equal(t, cl.ID, client.ID)
+	assert.Equal(t, cl.FIO, client.FIO)
+	assert.Equal(t, cl.Login, client.Login)
+	assert.Equal(t, cl.Birthday, client.Birthday)
+	assert.Equal(t, cl.Email, client.Email)
 }
 
 func Test_InsertClient_DeleteClient_ThenCheck(t *testing.T) {
-	// настройте подключение к БД
+	db, err := sql.Open("sqlite", "demo.db")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	cl := Client{
 		FIO:      "Test",
@@ -45,5 +92,18 @@ func Test_InsertClient_DeleteClient_ThenCheck(t *testing.T) {
 		Email:    "mail@mail.com",
 	}
 
-	// напиши тест здесь
+	rowID, err := insertClient(db, cl)
+	require.NotEmpty(t, rowID)
+	require.NoError(t, err)
+
+	client, err := selectClient(db, rowID)
+	require.NoError(t, err)
+	require.Equal(t, rowID, client.ID)
+
+	err = deleteClient(db, rowID)
+	require.NoError(t, err)
+
+	_, err = selectClient(db, client.ID)
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
 }
